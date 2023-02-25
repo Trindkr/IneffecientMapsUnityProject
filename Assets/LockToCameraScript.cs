378423812a83e0350b2bb800d1c82c32fc86ab95
Trindkr <maria.trindkaer@gmail.com>
2023-02-25 21:04:53 +0100

    Added AR scene with object fixed to camera movement.

---
+++ Assets/LockToCameraScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockToCameraScript : MonoBehaviour
+{
+
+    public Transform camTransform;
+    public Vector3 camPos;
+    private float distanceFromCamera = 19;
+
+    void LateUpdate()
+    {
+        //make object face the camera
+        transform.rotation = Camera.main.transform.rotation;
+
+        camPos = Camera.main.transform.position;
+        camTransform = Camera.main.transform;
+
+        Vector3 resultingPosition = camPos + camTransform.forward * distanceFromCamera;
+        transform.position = resultingPosition;
+    }
+}
+
