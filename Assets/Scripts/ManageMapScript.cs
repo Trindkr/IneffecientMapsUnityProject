90f5e88e7be3fccc1dd998fea5f9802292cbb86c
Trindkr <maria.trindkaer@gmail.com>
2023-02-27 00:35:08 +0100

    Added scaling system to map object.
    - Needs accelerator input

--- Assets/Scripts/ManageMapScript.cs
+++ Assets/Scripts/ManageMapScript.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class ManageMapScript : MonoBehaviour
 {
+    public float acceloration = 0.0f; //Defaults to 0, might need to change
+    public float duration = 10.0f; // 10 seconds
+    float startScaleSize = 300.0f;
+    float minSize = 10.0f;
+    float counter;
+    float scale;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        counter = 0.0f;
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+        if (transform.localScale.y <= minSize)
+        {
+            //What should happen when the map is at its smallest size?
+            Debug.Log("Map is at its smallest size");
+            break;
+        }
+
+        //acceloration = INSERT CODE HERE;
+        if(acceloration <= 0.0f && counter < duration)
+        {
+            counter += Time.deltaTime;
+            scale = Mathf.Lerp(startScaleSize, minSize, counter/duration);
+            transform.localScale = new Vector3(scale, scale, scale); // shrink from startsize to minsize;
+        }
+        //else if(acceloration > ????) //TODO when acceloration part is done
+        // {
+        //     counter = 0.0f;
+        //     transform.localScale = new Vector3(Mathf.Lerp(transform.localScale, startScaleSize, counter/duration), Mathf.Lerp(transform.localScale, startScaleSize, counter/duration), Mathf.Lerp(transform.localScale, startScaleSize, counter/duration)); // grow from current size to startsize;
+        // }
+       
     }
 }
