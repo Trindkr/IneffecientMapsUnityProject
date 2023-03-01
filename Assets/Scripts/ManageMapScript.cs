87905f400c181194727a28eea4f8b05635b2aabe
Trindkr <maria.trindkaer@gmail.com>
2023-03-01 23:16:16 +0100

    Map now moves around the screen
    It might leave the screen, if you don't follow it.
     - Will this be a problem?
    
    Maybe play into this, and add a feature that requires the map to be visible, in order for you to be able to catch it?

--- Assets/Scripts/ManageMapScript.cs
+++ Assets/Scripts/ManageMapScript.cs
@@ -8,13 +8,29 @@ public class ManageMapScript : MonoBehaviour
     public float duration = 10.0f; // 10 seconds
     float startScaleSize = 300.0f;
     float minSize = 10.0f;
-    float counter;
-    float scale;
+    float maxSize = 350.0f;
+    
+    float lastSmallestSize;
+
+
+    float countDown;
+    float countUp;
+    float scaleSmaller;
+    float scaleLarger;
+
+
+    public float speed = 2f; // The speed at which the object will move
+    public float range = 5f; // The range within which the object will move
+
+    private Vector3 targetPosition; // The position the object is flying towards
 
     // Start is called before the first frame update
     void Start()
     {
-        counter = 0.0f;
+        countDown = 0.0f;
+        countUp = 0.0f;
+
+        targetPosition = transform.position + Random.insideUnitSphere * range;
     }
 
     // Update is called once per frame
@@ -26,18 +42,38 @@ public class ManageMapScript : MonoBehaviour
             Debug.Log("Map is at its smallest size"); //remove this when done, otherwise it will spam the console (°ー°〃)
         }
 
-        //acceloration = INSERT CODE HERE;
-        if(acceloration <= 0.0f && counter < duration)
+        if (transform.localScale.y >= maxSize)
         {
-            counter += Time.deltaTime;
-            scale = Mathf.Lerp(startScaleSize, minSize, counter/duration);
-            transform.localScale = new Vector3(scale, scale, scale); // shrink from startsize to minsize;
+            //What should happen when the map is at its largest size?
+            Debug.Log("Map is at its largest size"); //remove this when done, otherwise it will spam the console (°ー°〃)
         }
-        //else if(acceloration > ????) //TODO when acceloration part is done
-        // {
-        //     counter = 0.0f;
-        //     transform.localScale = new Vector3(Mathf.Lerp(transform.localScale, startScaleSize, counter/duration), Mathf.Lerp(transform.localScale, startScaleSize, counter/duration), Mathf.Lerp(transform.localScale, startScaleSize, counter/duration)); // grow from current size to startsize;
-        // }
+
+        //acceloration = INSERT CODE HERE;
+        if(acceloration <= 0.0f && countDown < duration)
+        {
+            countUp = 0.0f; //reset
+            countDown += Time.deltaTime;
+            scaleSmaller = Mathf.Lerp(startScaleSize, minSize, countDown/duration);
+            transform.localScale = new Vector3(scaleSmaller, scaleSmaller, scaleSmaller); // shrink from startsize to minsize;
+            lastSmallestSize = transform.localScale.y;
+        }
+
+        if(acceloration >= 10.0f) //TODO when acceloration part is done
+        {
+            countDown = 0.0f; //reset
+            countUp += Time.deltaTime;
+            scaleLarger = Mathf.Lerp(lastSmallestSize, maxSize, countUp/duration);
+            transform.localScale = new Vector3(scaleLarger, scaleLarger, scaleLarger);
+        }
+
+        // If the object has reached its target position, set a new random target position
+        if (Vector3.Distance(transform.position, targetPosition) < 0.1f)
+        {
+            targetPosition = transform.position + Random.insideUnitSphere * range;
+        }
+
+        // Move the object towards its target position
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
        
     }
 }
