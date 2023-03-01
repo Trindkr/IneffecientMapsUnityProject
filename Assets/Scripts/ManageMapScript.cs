92489a94c2cff65cf4bdddc81a6d0b6f46bbd51f
Trindkr <maria.trindkaer@gmail.com>
2023-03-02 00:16:40 +0100

    Fixed some stuff, still a bit wonky, but it works.

--- Assets/Scripts/ManageMapScript.cs
+++ Assets/Scripts/ManageMapScript.cs
@@ -6,9 +6,9 @@ public class ManageMapScript : MonoBehaviour
 {
     public float acceloration = 0.0f; //Defaults to 0, might need to change
     public float duration = 10.0f; // 10 seconds
-    float startScaleSize = 300.0f;
-    float minSize = 10.0f;
-    float maxSize = 350.0f;
+    float startScaleSize = 0.4f;
+    float minSize = 0.01f;
+    float maxSize = 0.6f;
     
     float lastSmallestSize;
 
@@ -19,21 +19,23 @@ public class ManageMapScript : MonoBehaviour
     float scaleLarger;
 
 
-    public float moveSpeed = 2.0f;
-    public float borderSize = 50.0f;
-    Camera mainCamera;
+    public float moveSpeed = 0.1f;
+    public float radius = 1f;
 
-    private Vector3 targetPosition; // The position the object is flying towards
+    private Vector3 center;
+    private Vector3 localPosition;
+    private Vector3 targetPosition;
 
     // Start is called before the first frame update
     void Start()
     {
-        mainCamera = Camera.main;
+    
         countDown = 0.0f;
         countUp = 0.0f;
 
-        targetPosition = GetRandomPositionWithinBorder();
-    }
+        center = transform.parent.position;
+        localPosition = transform.localPosition;
+        targetPosition = localPosition;    }
 
     // Update is called once per frame
     void LateUpdate()
@@ -68,34 +70,24 @@ public class ManageMapScript : MonoBehaviour
             transform.localScale = new Vector3(scaleLarger, scaleLarger, scaleLarger);
         }
 
-        // If the object has reached its target position, set a new random target position
-        if (Vector3.Distance(transform.position, targetPosition) < 0.1f)
-        {
-            targetPosition = GetRandomPositionWithinBorder();
-        }
+        // Generate a random direction and move the target position along it
+        Vector3 randomDirection = Random.insideUnitSphere;
+        targetPosition += randomDirection * moveSpeed * Time.deltaTime;
 
-        // Move the object towards its target position
-        Vector3 moveDirection = (targetPosition - transform.position).normalized;
-        float distanceToCamera = Vector3.Distance(transform.position, mainCamera.transform.position);
-        transform.position += moveDirection * moveSpeed * Time.deltaTime;
-        transform.position = mainCamera.transform.position + (transform.position - mainCamera.transform.position).normalized * distanceToCamera;
+        // Clamp the target position within the parent object
+        Vector3 clampedPosition = targetPosition;
+        clampedPosition.x = Mathf.Clamp(clampedPosition.x, -radius, radius);
+        clampedPosition.y = Mathf.Clamp(clampedPosition.y, -radius, radius);
+        clampedPosition.z = Mathf.Clamp(clampedPosition.z, -radius, radius);
+
+        // Move the child object towards the clamped target position
+        Vector3 newPosition = Vector3.MoveTowards(transform.localPosition, clampedPosition, moveSpeed * Time.deltaTime);
+        transform.localPosition = newPosition;
        
+
+        
     }
 
-     Vector3 GetRandomPositionWithinBorder()
-    {
-        // Get a random point within the border by using Random.insideUnitSphere
-        Vector3 randomPoint = Random.insideUnitSphere * borderSize;
-
-        // Make sure the random point is within the border by clamping it to the border's bounds
-        randomPoint.x = Mathf.Clamp(randomPoint.x, -borderSize, borderSize);
-        randomPoint.y = Mathf.Clamp(randomPoint.y, -borderSize, borderSize);
-        randomPoint.z = Mathf.Clamp(randomPoint.z, -borderSize, borderSize);
-
-        // Set the random point's height to match the object's current height
-        randomPoint.y = transform.position.y;
-
-        return randomPoint;
-    }
+   
 
 }
